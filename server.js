5859fa47c6208559ba65f86fabb9c90f61615837
Ryan Harris <harris.ucla@gmail.com>
2020-04-08 20:54:17 -0700

    server.js serving static pages

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  express.static("public", {
+    extensions: "html"
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));

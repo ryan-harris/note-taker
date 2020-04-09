945935c95179cc7abf20aae813d891b699b3614c
Ryan Harris <harris.ucla@gmail.com>
2020-04-08 21:38:22 -0700

    create api endpoints, get /api/notes endpoint implemented

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const express = require("express");
+const fs = require("fs");
+
+const NOTES_FILE = path.join(__dirname, "db", "db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +14,23 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(NOTES_FILE, (err, data) => {
+    if (err) throw err;
+
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  // add new note
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  // delete note id
+  console.log(`Deleting note ${req.param.id}`);
+});
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "index.html"))
